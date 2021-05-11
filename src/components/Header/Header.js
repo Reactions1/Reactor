8bdf4ba973de8faea9c1779d8ede7bc1168264d5
ashc901 <ac.carpenter193@gmail.com>
2021-05-11 13:00:33 -0500

    Add some styling
    Experimenting with navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const authenticatedOptions = (
     <Nav.Link href="#/">Home</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-    <Nav.Link href="#posts">Posts</Nav.Link>
+    <Nav.Link href="#posts">Your Posts</Nav.Link>
   </Fragment>
 )
 
@@ -24,19 +24,36 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#">
+  <Navbar variant="light" bg="primary" expand="md">
+    <Navbar.Brand class="brand" variant="light" href="#" >
       Reactor
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { user && <span className="navbar-text mr-2" variant="light">Welcome, {user.email}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
   </Navbar>
 )
+// const Header = ({ user }) => (
+//   <Navbar bg="primary" expand="lg">
+//   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//   <Navbar.Collapse id="basic-navbar-nav">
+//     <Nav className="mr-auto">
+//       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//   </Navbar.Collapse>
+// </Navbar>
+// )
 
 export default Header
