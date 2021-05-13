da742c640ee429c4490840fa1ca99050ddf10091
ashc901 <ac.carpenter193@gmail.com>
2021-05-13 10:30:29 -0500

    Update config link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://vast-dawn-05766.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
